0cecd1e761cd08984ac857c15f8fb72d7bf9ecf1
Iulian Preda <iulian.preda.dev@protonmail.com>
2023-06-25 21:39:40 +0200

    Removed git submodules in favor of cloning specific commits

---
+++ scripts/git/clone-ffmpeg.js
@@ -0,0 +1,12 @@
+import { ffmpegGitUrl, ffmpegPath } from '../vars.js';
+import { getAllTags, getLatestTag, gitClone } from './git-utils.js';
+
+const allTags = await getAllTags(ffmpegGitUrl);
+
+if (allTags) {
+  const latestFfmpegReleaseTag = getLatestTag(allTags);
+
+  if (latestFfmpegReleaseTag) {
+    await gitClone(ffmpegGitUrl, latestFfmpegReleaseTag, ffmpegPath);
+  }
+}
---
+++ scripts/git/git-utils.js
@@ -0,0 +1,51 @@
+import { useShell } from '../shared.js';
+
+/**
+ * @param {string} url
+ * @param {string} branch
+ * @param {string} dest
+ * @param {number} depth
+ * @returns {Promise<string|undefined>}
+ */
+export function gitClone(url, branch, dest, depth = 1) {
+  return useShell(`git clone --branch ${branch} --depth ${depth} ${url} ${dest}`);
+}
+
+/**
+ * @param {any} repoUrl
+ * @returns {Promise<string[]|undefined>}
+ */
+export function getAllTags(repoUrl) {
+  return useShell(`git ls-remote --tags --refs ${repoUrl}`).then((res) => res?.split('\n'));
+}
+
+/**
+ * @param {any[]} tags
+ * @param {RegExp} regex
+ * @returns {string|undefined}
+ */
+export function getLatestTag(tags, regex = /^n\d+\.\d+(?:\.\d+)?$/) {
+  const filteredTags = tags
+    .map((tag) => tag.split('/').pop())
+    .filter((tag) => regex.test(tag))
+    .sort((a, b) => versionCompare(a, b));
+
+  return filteredTags.pop();
+}
+
+/**
+ * @param {string} a
+ * @param {string} b
+ */
+function versionCompare(a, b) {
+  const aParts = a.split('.').map(Number);
+  const bParts = b.split('.').map(Number);
+
+  for (let i = 0; i < aParts.length; i++) {
+    if (aParts[i] !== bParts[i]) {
+      return aParts[i] - bParts[i];
+    }
+  }
+
+  return 0;
+}
